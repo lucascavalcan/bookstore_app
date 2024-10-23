61728779eaa2bc698019972244ce94b9059f0a37
Lucas Cavalcanti <lcavalcantidearaujo@gmail.com>
2024-10-22 22:41:25 -0300

    first commit

---
+++ src/test/java/com/bookstore/bookstore_app/BookstoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookstore_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
