dbe821868bfd191507fe931671cbf58e44b1562b
Lucas Cavalcanti <lcavalcantidearaujo@gmail.com>
2024-10-23 23:19:41 -0300

    class PublisherModel

--- src/main/java/com/bookstore/bookstore_app/models/PublisherModel.java
+++ src/main/java/com/bookstore/bookstore_app/models/PublisherModel.java
@@ -1,4 +1,21 @@
 package com.bookstore.bookstore_app.models;
 
-public class PublisherModel {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PUBLISHER")
+@Data
+public class PublisherModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
 }
